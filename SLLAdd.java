c7d14a50d2d2670c9f939bc58082c9a3f0f9fac9
ivanab <59364280+ivanab@users.noreply.github.com>
2022-07-09 01:29:09 +0200

    Add nodes to list

---
+++ SLLAdd.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class SLLAdd {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        NumbersList list = new NumbersList();
+        System.out.println("How many numbers do you want in the list?");
+        int n = in.nextInt();
+        System.out.println("Input numbers:");
+        for (int i = 0; i < n; i++) {
+            int num = in.nextInt();
+            list.addNode(num);
+        }
+        System.out.println(list.toString());
+    }
+}
+
+class NumbersList {
+    class Node {
+        int number;
+        Node next;
+
+        public Node(int n) {
+            number = n; 
+            Node next = null;
+        }
+
+        public String toString() {
+            return number + "";
+        }
+    }
+    // Points to the first node.
+    Node first;
+
+    public NumbersList() {
+        first = null;
+    }
+
+    public void addNode(int n) {
+        Node node = new Node(n);
+        node.next = first;
+        first = node;
+    }
+
+    public String toString() {
+        String result = "List: [ ";
+        Node current = first;
+        while (current != null) {
+            result += current.number + " ";
+            current = current.next;
+        }
+        return result + "]";
+    }
+}
