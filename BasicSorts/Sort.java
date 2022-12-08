9c4a5de587ddd56618fbd5fe53328b60b2fe8ad1
ivanab <59364280+ivanab@users.noreply.github.com>
2022-12-08 20:36:20 +0100

    Selection sort

--- BasicSorts/Sort.java
+++ BasicSorts/Sort.java
@@ -1,6 +1,6 @@
 public class Sort {
 
-    // Exchange or Bubble sort - the most inefficient way to sort elemens.
+    // Exchange or Bubble sort - the most inefficient way to sort elements.
     public static <T extends Comparable<T>> void exchangeSort(T[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = arr.length - 1; j > i; j--) {
@@ -13,3 +13,21 @@ public class Sort {
         }
     }
 }
+
+// Selection sort - when it's easier to compare elements than to move them
+public static <T extends Comparable<T>> void selectionSort(T[] arr) {
+    for (int i = 0; i < arr.length - 1; i++) {
+      int minIndex = i;
+      for (int j = i + 1; j < arr.length; j++) {
+        if (arr[j].compareTo(arr[minIndex]) < 0) {
+          minIndex = j;  
+        }
+      }
+      if (minIndex != i) {
+        T tmp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = tmp;
+      }
+    } 
+  }
+  
